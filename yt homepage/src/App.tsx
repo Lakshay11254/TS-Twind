eb751de1fbb10acf48c203058c0e5ebfa4302442
Lakshay <lakshay.sbp@gmail.com>
2023-10-17 14:30:32 +0530

    Stickybar is done

--- yt homepage/src/App.tsx
+++ yt homepage/src/App.tsx
@@ -1,9 +1,24 @@
+import {useState} from "react";
 import { PageHeader } from "./layouts/PageHeader";
+import {CategoryPills} from "./components/CategoryPills";
+import { categories } from "./data/home";
 
 export default function App() {
+    const [selectedCategory, setSelectedCategory] = 
+    useState(categories[0])
+
 return (
 <div className="flex max-h-screen  flex-col">
  <PageHeader/>
+<div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
+    <div>Sidebar</div>
+    <div className="sticky top-0 bg-white z-10 pb-4">
+        <CategoryPills categories={categories}
+        selectedCategory = {selectedCategory}
+        onSelect={setSelectedCategory}/>
+    </div>
+    </div>
 </div>
+
 )
 }
