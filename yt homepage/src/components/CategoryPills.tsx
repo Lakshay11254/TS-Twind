29e78ae6a59130e8ec58100889aaf619c262049a
Lakshay <lakshay.sbp@gmail.com>
2023-10-17 16:52:49 +0530

    left topbar is done without js

--- yt homepage/src/components/CategoryPills.tsx
+++ yt homepage/src/components/CategoryPills.tsx
@@ -1,8 +1,9 @@
+import { ChevronLeft } from "lucide-react";
 import { Button } from "./Button";
 
 type CategoryPillProps = {
   categories: string[];
-  selectedCategory: string
+  selectedCategory: string;
   onSelect: (category: string) => void;
 };
 
@@ -24,6 +25,15 @@ export function CategoryPills({
             {category}
           </Button>
         ))}
+      </div>
+      <div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-24 h-full">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="h-full aspect-square w-auto p-1.5"
+        >
+          <ChevronLeft />
+        </Button>
       </div>
     </div>
   );
